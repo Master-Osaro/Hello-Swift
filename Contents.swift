addb15c34817371723fb3abd50edc85e75adc524
Osaro Iyoha <saro.iyoha@gmail.com>
2018-01-08 09:39:45 +0100

    Update Contents.swift

--- Contents.swift
+++ Contents.swift
@@ -4,3 +4,8 @@ import UIKit
 
 var str = "Hello playground, hello world!"
 print(str)
+//use var to declare variables, let to declare constants
+let myTemp = 50
+//casting te integer myTemp to string
+let temperature = String(myTemp)
+print (temperature)
